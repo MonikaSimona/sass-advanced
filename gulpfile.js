293e3666e7cfccfeb3f04a22041dfcffaedf15b2
Simona Simonovska <simona.simonovska@atis.al>
2022-01-28 16:03:06 +0100

    functions, mixins, loops and other

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
     return src('katara/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['katara/**/*.scss'], buildStyles)
+    watch(['katara/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
